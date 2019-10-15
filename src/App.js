9ed250c0a2bf530cd7897eb5ece988a5f19b7e0f
andrewrayan <andrewrayan@gmail.com>
2019-10-15 23:11:18 +0530

    Applying dynamic styles using className

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import UserComponent from './Users/Users';
-import Product from './Product/product';
-import './App.css';
+import React, { Component } from "react";
+import UserComponent from "./Users/Users";
+import Product from "./Product/product";
+import "./App.css";
 
 class App extends Component {
   state = {
-    productList: [{
-      product: 'iphone', key: "1"
-    }, { product: 'redmi', key: "2" }, { product: 'oppo', key: "3" }, { product: 'vivo', key: "4" }]
+    productList: [
+      {
+        product: "iphone",
+        key: "1"
+      },
+      { product: "redmi", key: "2" },
+      { product: "oppo", key: "3" },
+      { product: "vivo", key: "4" }
+    ]
   };
 
   changeProduct = (productName, oldValue) => {
@@ -17,37 +23,45 @@ class App extends Component {
       this.setState(productList);
       console.log(oldValue);
     }
+  };
 
-  }
-
-  removeProduct = (productIndex) => {
-    console.log('index position', productIndex);
+  removeProduct = productIndex => {
+    console.log("index position", productIndex);
     const productList = this.state.productList.splice(productIndex, 1);
     this.setState(productList);
-  }
+  };
 
-  printValue = (value) => {
-    console.log('Value is ', value);
-  }
+  printValue = value => {
+    console.log("Value is ", value);
+  };
 
   render() {
     console.log(this.state.productList);
     const products = this.state.productList.map((value, index) => {
-      return (<Product key={value.key} product={value.product} onClick={() => this.printValue(value)}
-        indexPosition={index} removeFunction={this.removeProduct}
-      />);
+      return (
+        <Product
+          key={value.key}
+          product={value.product}
+          onClick={() => this.printValue(value)}
+          indexPosition={index}
+          removeFunction={this.removeProduct}
+        />
+      );
     });
 
     return (
       <div className="App">
         <header>
-          <p>
-            This is my first react application.
-        </p>
+          <p>This is my first react application.</p>
         </header>
         <div>
           {products}
-          <button onClick={this.changeProduct.bind(this, 'Samsung', 'iphone')}>Change product name</button>
+          <button
+            className="success"
+            onClick={this.changeProduct.bind(this, "Samsung", "iphone")}
+          >
+            Change product name
+          </button>
         </div>
         <UserComponent />
       </div>
--- src/Product/product.js
+++ src/Product/product.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
-
-const Product = (props) => {
-    return (
-        <div>
-            <p onClick={() => props.removeFunction(props.indexPosition)}>Hi {props.product}</p>
-        </div>
-    );
-}
+import React, { useState } from "react";
+import "./product.css";
+const Product = props => {
+  const applyClass = props.indexPosition % 2 == 0 ? "even" : "odd";
+  return (
+    <div>
+      <p
+        className={applyClass}
+        onClick={() => props.removeFunction(props.indexPosition)}
+      >
+        Hi {props.product}
+      </p>
+    </div>
+  );
+};
 
 export default Product;
